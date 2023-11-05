6cd931bbc7dbe66deb2605afe70fe6a1c9f48da2
abhigoel25 <89696508+abhigoel25@users.noreply.github.com>
2023-11-05 00:07:23 -0400

    Adjusting pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageTele.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 public class CenterStageTele extends OpMode{
@@ -15,23 +16,21 @@ public class CenterStageTele extends OpMode{
     DcMotor intake;
 
     double slidePositionTarget = 0.0;
-    double previousLeftError = 0.0;
-    double previousRightError = 0.0;
-    double leftInt = 0.0;
-    double rightInt = 0.0;
-
     // Gains for slides, to be tuned
     double kP = 1.0;
     double kI = 1.0;
     double kD = 1.0;
-
+    double integralSum = 0.0;
+    private double lastError = 0.0;
+    ElapsedTime timer = new ElapsedTime();
     DcMotor ls;
     DcMotor rs;
-    /*
+
     Servo armRight;
     Servo armLeft;
     Servo pRight;
     Servo pLeft;
+    /*
     Servo intakeRight;
     Servo intakeLeft; */
     @Override
@@ -43,21 +42,22 @@ public class CenterStageTele extends OpMode{
         intake = hardwareMap.get(DcMotor.class, "motorIntake");
         ls = hardwareMap.get(DcMotor.class, "motorLS");
         rs = hardwareMap.get(DcMotor.class, "motorRS");
-/*
+
         armLeft = hardwareMap.get(Servo.class, "armLeft");
         armRight = hardwareMap.get(Servo.class, "armRight");
         pRight = hardwareMap.get(Servo.class, "plungerRight");
         pLeft = hardwareMap.get(Servo.class, "plungerLeft");
-        intakeRight = hardwareMap.get(Servo.class, "intakeRight");
+
+        /* intakeRight = hardwareMap.get(Servo.class, "intakeRight");
         intakeLeft = hardwareMap.get(Servo.class, "intakeLeft");
 
         intakeLeft.setPosition(0.0);
-        intakeRight.setPosition(0.0); */
+        intakeRight.setPosition(0.0);
 
-//        rf.setDirection(DcMotorSimple.Direction.REVERSE);
-//        rb.setDirection(DcMotorSimple.Direction.REVERSE);
-//        lb.setDirection(DcMotorSimple.Direction.REVERSE);
-//        lf.setDirection(DcMotorSimple.Direction.REVERSE);
+        rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        rb.setDirection(DcMotorSimple.Direction.REVERSE);
+        lb.setDirection(DcMotorSimple.Direction.REVERSE);
+        lf.setDirection(DcMotorSimple.Direction.REVERSE); */
     }
 
     @Override
@@ -68,26 +68,11 @@ public class CenterStageTele extends OpMode{
         double r = -gamepad1.right_stick_x; //pivot and rotation
 
         double slidesScalar = 1.0; // factor for motor power
-        slidePositionTarget += slidesScalar * gamepad2.right_stick_y;
+        slidePositionTarget += slidesScalar * gamepad2.left_stick_y;
         telemetry.addData("Slide target: ", slidePositionTarget);
 
-        // this calculates error for both slides and assigns a value to the duration of each loop cycle
-        double leftError = slidePositionTarget - 0.0; //<-- PLACEHOLDER, THIS SHOULD BE KNOWN FROM AN ENCODER, also could be the opposite of this
-        double rightError = slidePositionTarget - 0.0; //<-- PLACEHOLDER, THIS SHOULD BE KNOWN FROM AN ENCODER, also could be the opposite of this
-        double loopDuration = 1.0; //<-- PLACEHOLDER, I DON'T KNOW HOW WE GET THIS
-
-        // this should calculate the P I and D values based on error, but should be checked
-        double propL =  leftError;
-        double derivL = leftError - previousLeftError / loopDuration;
-        leftInt+=leftError * loopDuration;
-        double propR =  rightError;
-        double derivR = rightError - previousRightError / loopDuration;
-        rightInt+=rightError * loopDuration;
-
-        // power slide motors
-        ls.setPower(kP*propL + kD*derivL + kI*leftInt);
-        rs.setPower(kP*propR + kD*derivR + kI*rightInt);
-
+        ls.setPower(PIDControl(slidePositionTarget, ls.getCurrentPosition()));
+        rs.setPower(PIDControl(slidePositionTarget, rs.getCurrentPosition()));
 
         boolean turtle = false;
         if(gamepad1.left_trigger > 0.5 || gamepad1.right_trigger > 0.5){
@@ -117,7 +102,7 @@ public class CenterStageTele extends OpMode{
         double postRB = preRB/max;
         double postLB = preLB/max;
 
-       /* if(gamepad2.b){
+        if(gamepad2.b){
             armRight.setPosition(0.0);
             armLeft.setPosition(0.0);
         }
@@ -126,7 +111,7 @@ public class CenterStageTele extends OpMode{
             armRight.setPosition(0.4);
             armLeft.setPosition(0.4);
         }
-        */
+
         intake.setPower((gamepad2.right_bumper)? -0.95 : 0);
         intake.setPower((gamepad2.left_bumper)? 0.6 : 0);
 
@@ -136,12 +121,21 @@ public class CenterStageTele extends OpMode{
         telemetry.addData("lb", postLB);
 
         telemetry.addData("intake", intake.getPower());
-       /* telemetry.addData("armRight", armRight.getPosition());
-        telemetry.addData("armLeft", armLeft.getPosition());
-         */
+        telemetry.addData("armRight", "Position: " + armRight.getPosition());
+        telemetry.addData("armLeft", "Position: " + armLeft.getPosition());
         telemetry.update();
     }
 
+    public double PIDControl(double reference, double state){
+        double error = reference - state;
+        integralSum += error * timer.seconds();
+        double derivative = (error-lastError) / timer.seconds();
+        lastError = error;
+        timer.reset();
+
+        double power = (error * kP) + (derivative * kD) + (integralSum*kI);
+        return power;
+    }
     @Override
     public void stop(){
         super.stop();
