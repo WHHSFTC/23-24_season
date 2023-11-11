5144862796c88fddccf11fa576f9ae296a674b97
abhigoel25 <89696508+abhigoel25@users.noreply.github.com>
2023-11-10 19:31:15 -0500

    adding score auto files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScoreBlue.java
@@ -0,0 +1,27 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous (preselectTeleOp = "CenterStageTele")
+public class ScoreBlue extends OpMode{
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loop(){
+
+    }
+
+    public void stop(){
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScoreRed.java
@@ -0,0 +1,27 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous (preselectTeleOp = "CenterStageTele", group = "_official")
+public class ScoreRed extends OpMode{
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loop(){
+
+    }
+
+    public void stop(){
+
+    }
+}
